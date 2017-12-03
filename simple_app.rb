ef48aa9543fefba70ddcdf9229db9d5d21885c72
Madelene Campos <madelenecampos@gmail.com>
2017-12-03 15:21:38 -0500

    Initial commit, including file setup and homepage

---
+++ simple_app.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+
+get "/home" do 
+  send_file 'intro.html'
+ 
+# a main section which at first is blank
+# a numeric count down from 3 to 1 in the middle of the main section
+# at the end of the countdown make an ajax request to your /code endpoint to get the source code of your app
+# display your source code in the main section with proper formatting and syntax highlighting
+	
+end
+
+get "/code" do
+
+# curl http://your-app.herokuapp.com/code
+ 
+# # …and receive the JSON response:
+     
+# { "code": "the source code of your app" }
+
+end
