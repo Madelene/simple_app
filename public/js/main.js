37790842740aeba7c42fcd482cfb2b3cf3cc3222
Madelene Campos <madelenecampos@gmail.com>
2017-12-03 19:54:30 -0500

    Getting close with displaying content from ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,9 @@
 function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
+  var timer = duration, seconds;
   setInterval(function () {
     seconds = parseInt(timer % 60, 10);
-
     seconds = seconds;
-
     display.textContent = seconds;
-
     if (--timer < 0) {
       timer = duration;
     }
@@ -18,5 +15,16 @@ window.onload = function () {
     display = document.querySelector('#time');
   startTimer(threeSeconds, display);
 };
+
+
+$(document).ready(function(){
+  $("section").click(function(){
+    $.ajax({
+      url: "/code", success: function(result){
+      $("#timer").html(result);
+      }
+    });
+  });
+});
  
 
